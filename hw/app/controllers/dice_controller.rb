cb7e4964e1c00ccc4e77fc6eb9fb39d410a78dbc
bshaps <bshaps1984@gmail.com>
2015-04-15 22:57:47 -0500

    Challenge #4 setup

---
+++ hw/app/controllers/dice_controller.rb
@@ -0,0 +1,6 @@
+class DiceController < ActionController::Base
+
+	def index
+	
+	end
+end
--- hw/config/routes.rb
+++ hw/config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get '/contact', controller: 'contact', action: 'index'
   get '/contact_submitted', controller: 'contact_submitted', action: 'index'
   get '/rps', controller: 'rps', action: 'play'
+  get '/dice', controller: 'dice', action: 'index'
 end
